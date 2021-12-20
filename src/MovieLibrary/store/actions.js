464043e44446b032d841c2b0489d0cf63516242a
Janku98 <facundojankunas@hotmail.com.ar>
2021-12-20 20:19:07 -0300

    Infinite Scroll ready

--- src/MovieLibrary/store/actions.js
+++ src/MovieLibrary/store/actions.js
@@ -5,7 +5,7 @@ import API_KEY from '../../KEY';
 export function fetchMovies(pageNumber) {
   return async function (dispatch){
     const res = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=${pageNumber}`);
-    console.log("RES", res.data.results)
+    console.log("RES", res.data)
     dispatch({
     type: actions.FETCH_MOVIES,
     payload: res.data.results
